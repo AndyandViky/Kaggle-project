6578a24929dd338e1d0baa03ae72060c6a068e4a
Andy <andy_viky@163.com>
2019-08-19 10:40:35 +0800

    structure:add config file

---
+++ Digit-Recongnizer/config.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+"""
+@author: andy
+@contact: andy_viky@163.com
+@github: https://github.com/AndyandViky
+@csdn: https://blog.csdn.net/AndyViky
+@file: config.py
+@time: 2019/8/19 上午10:31
+@desc: 
+"""
+
+import os
+
+Digit_DIR = os.path.dirname(os.path.abspath(__file__))
+
+Data_DIR = os.path.join(Digit_DIR, 'datasets')
+
+Model_DIR = os.path.join(Digit_DIR, 'model')
